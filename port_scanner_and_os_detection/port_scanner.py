307171ecb706be0ad366d96d8ace864892170ccc
FIRST_NAME LAST_NAME <charantm8787@gmail.com>
2024-11-23 23:47:22 +0530

    .

--- port_scanner_and_os_detection/port_scanner.py
+++ port_scanner_and_os_detection/port_scanner.py
@@ -1,12 +1,15 @@
 import pyfiglet
 import sys
 import socket
+import time
+import threading
 from datetime import datetime
 from scapy.all import IP, TCP, sr1, conf
 
 #ASCII is american standard code of information interchange
 #th emodulw pyfiglet is used to generate grafical image of the given string and 
 # figlet_format is a key word in pyfiglet library it convert the string that we provided and turns into ASCII art.
+
 ascii_banner = pyfiglet.figlet_format("PORT SCANNER",font="slant")
 print(ascii_banner)
 
@@ -28,7 +31,7 @@ def detect_os(ip):
     try:
 		#sends the packet to the particular IP address
 		#creates a packets for a destined IP and it is attach with TCP, and dport=80(http) is used for to
-		#send the packets to the specfied port caz port 80 which is commonly opened in many devices.
+		#send the packets to the specfied port cuz port 80 which is commonly opened in many devices.
         pkt = IP(dst=ip)/TCP(dport=80, flags="S")  # flags='s' create SYN packet
         resp = sr1(pkt, timeout=2, verbose=0) #send the packets to the IP,  if the reponse from the target address within the 2sec timout 
 		#and the repose id stored in the resp, if not it return None.
@@ -50,40 +53,112 @@ def detect_os(ip):
 
 #calling the func
 detect_os(target)
-
+print("-"*55)
 try:
-	start_port = int(input("Enter the starting port: "))
-	end_port = int(input("Enter the ending port: "))
-	print("-"*54)
-	print(f"Scanning ports from {start_port} to {end_port} on Target {target}")
-	print("-"*54)
-	print("Waiting for response.....")
-	# will scan ports between 1 to 65,535
-	for port in range(start_port,end_port+1):
-		#AF_INET means ipv4 address and SOCK_STREAM specifies the TCP
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	print("1. Search for Only Opened Ports (Slow)\n2. search for Ports in Range (Recommended)\n3. Quick Scan (Fast)")
+	choice = int(input("[1/2/3]: "))
+	if choice == 1:
+		print("Estimated Time to scan (1024) ports is 1-2min!")
+		print("Waiting for response........\n")
+		# will scan ports between 1 to 1024 common ports and registered ports
+		start = time.time()
+		for port in range(1,1024):
+			#AF_INET means ipv4 address and SOCK_STREAM specifies the TCP
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-		#it set the time for any type of operation should be done in 2 seconds
-		socket.setdefaulttimeout(2)
-		
-		# it try to connect to the specific IP address and port if it connects it return the vlaue '0'
-		result = s.connect_ex((target,port))
+			#it set the time for any type of operation should be done in 2 seconds
+			socket.setdefaulttimeout(2)
+			
+			# it try to connect to the specific IP address and if it connects it return the vlaue '0'
+			result = s.connect_ex((target,port))
 
-		with open("scan_result.txt","a") as f:
-			if result ==0:
-				service = socket.getservbyport(port, "tcp")
-				print(f"Port {port} is open ({service})")
-				f.write(f"\nPort {port} is open ({service}) on the host {target}")
-			else:
-				print(f"port {port} is closed or filtered")
-			s.close()
+			with open("scan_result.txt","a") as f:
+				if result ==0:
+					service = socket.getservbyport(port, "tcp")
+					print(f"Port {port} is open ({service})")
+					f.write(f"\nPort {port} is open ({service}) on the host {target}")
+				s.close()
+		if result != 0:
+			print(f"-->No ports are opened on {target}<--")
+		end =time.time()
+		print(f"-->Time taken {end-start:.2f} seconds!<--")
+
+	elif choice == 2:
+		start_port_1 = int(input("Enter the starting port: "))
+		end_port_1 = int(input("Enter the ending port: "))
+		print("-"*54)
+		print(f"Scanning ports from {start_port_1} to {end_port_1} on Target {target}")
+		print("-"*54)
+		print("Waiting for response.....")
+		# will scan ports between 1 to 65,535
+		starttime=time.time()
+		for port in range(start_port_1,end_port_1+1):
+			#AF_INET means ipv4 address and SOCK_STREAM specifies the TCP
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+			#it set the time for any type of operation should be done in 2 seconds
+			socket.setdefaulttimeout(2)
+			
+			# it try to connect to the specific IP address and port if it connects it return the vlaue '0'
+			result = s.connect_ex((target,port))
+
+			with open("scan_result.txt","a") as f:
+				if result ==0:
+					service = socket.getservbyport(port, "tcp")
+					print(f"Port {port} is open ({service})")
+					f.write(f"\nPort {port} is open ({service}) on the host {target}")
+				else:
+					print(f"port {port} is closed or filtered")
+				s.close()
+		endtime=time.time()
+		print(f"-->Time take {endtime-starttime:.2f} seconds<--")
+
+	elif choice==3:
+		start_port_2 = int(input("Enter the starting port: "))
+		end_port_2 = int(input("Enter the end port: "))
+		#its a normal port scanning code you would see in the choice 1 section
+		def scan_port(ip, port):
+			try:
+				s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				s.settimeout(1)
+				result = s.connect_ex((ip, port))
+				if result == 0:
+					print(f"Port {port} is OPEN")
+				else:
+					print(f"Port {port} is CLOSED")
+				s.close()
+			except socket.error as e:
+				print(f"Error scanning port {port}: {e}")
+
+		def quick_scan(ip, port_range):
+			print(f"Starting quick scan on {ip}")
+			
+			#creates a list to store the threads
+			threads = []
+			for port in range(port_range[0], port_range[1] + 1):
+				thread = threading.Thread(target=scan_port, args=(ip, port)) #it createa a new thread by using threading.Thread 
+				threads.append(thread)
+				thread.start()
+			
+			# Wait for all threads to complete
+			for thread in threads:
+				thread.join()
+		port_range = (start_port_2,end_port_2)
+		ip=target
+		quick_scan(ip, port_range)
+	else:
+		print("Invalid Input")
+		sys.exit(1)
 
 except KeyboardInterrupt:
-		print("\n Exiting Program !!!!")
+		print("\nKeyboard Interrupt")
+		print("Exiting Program !!!!")
 		sys.exit()
+#if host name is not able to convert into the ip gaierror will occur
 except socket.gaierror:
-		print("\n Hostname Could Not Be Resolved !!!!")
+		print("\n Hostname Couldn't be Convert into Ip (or) Ivalid Host name !!!!")
 		sys.exit()
+#if th target server in not responded
 except socket.error:
-		print("\n Server not responding !!!!")
+		print("\n Target Server not responding !!!!")
 		sys.exit()
